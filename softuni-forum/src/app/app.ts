4f809375e860e0ba78f849fd23b3cd313b88baa1
Milen03 <milen.atanasovv03@gmail.com>
2025-07-06 22:01:54 +0300

    app.html pasts

--- softuni-forum/src/app/app.ts
+++ softuni-forum/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer, Header } from './shared/components';
+import { ThemeBoard } from "./features/themes/theme-board/theme-board";
+import { PostBoard } from "./features/posts/post-board/post-board";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, ThemeBoard, PostBoard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
