50c23b462f28796470ed525bc5852f8d4509ac82
Milen03 <milen.atanasovv03@gmail.com>
2025-07-18 19:06:42 +0300

    Set Home Componets

--- softuni-forum/src/app/app.ts
+++ softuni-forum/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer, Header } from './shared/components';
-import { ThemeBoard } from "./features/themes/theme-board/theme-board";
-import { PostBoard } from "./features/posts/post-board/post-board";
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ThemeBoard, PostBoard],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
