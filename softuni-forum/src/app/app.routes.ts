996065b03187a51c1c6a755462665b07d5da1db7
Milen03 <milen.atanasovv03@gmail.com>
2025-07-18 20:08:50 +0300

    Set login componet

--- softuni-forum/src/app/app.routes.ts
+++ softuni-forum/src/app/app.routes.ts
@@ -11,5 +11,9 @@ export const routes: Routes = [
         path: 'home',
        loadComponent: () => import('./features/home/home').then(c => c.Home)
             
+    },
+    {
+        path:'login',
+        loadComponent: () => import('./features/auth/login/login').then(c=> c.Login)
     }
 ];
