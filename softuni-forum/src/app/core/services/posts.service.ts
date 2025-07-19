77ce45a63693ca7caa791ea9e2b2e8df288b39d0
Milen03 <milen.atanasovv03@gmail.com>
2025-07-19 19:51:27 +0300

    Uppdate postService

--- softuni-forum/src/app/core/services/posts.service.ts
+++ softuni-forum/src/app/core/services/posts.service.ts
@@ -8,13 +8,21 @@ import { Observable } from "rxjs/internal/Observable";
 })
 
 export class PostService{
-private  apiUrl = 'http://localhost:3000/api/posts?limit={0}';
+ private getPostsApiUrl = 'http://localhost:3000/api/posts?limit={0}';
+    private createPostApiUrl = 'http://localhost:3000/api/posts';
 
-constructor(private httpClient:HttpClient){
+    constructor(private httpClient: HttpClient) {}
 
-}
+    getPosts(limit: number = 5): Observable<Post[]> {
+        return this.httpClient.get<Post[]>(this.getPostsApiUrl.replace('{0}', limit.toString()));
+    }
 
-getPosts(limit:number = 5):Observable<Post[]>{
-        return this.httpClient.get<Post[]>(this.apiUrl.replace('{0}',limit.toString()))
+    createPost(themeName: string, postText: string): Observable<Post> {
+        const body = JSON.stringify({ themeName, postText });
+        return this.httpClient.post<Post>(this.createPostApiUrl, body, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
     }
 }
