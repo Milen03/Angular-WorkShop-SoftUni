e17daac264accd342f878c27b9c4e785a16ff69d
Milen03 <milen.atanasovv03@gmail.com>
2025-07-27 19:40:57 +0300

    Add login validar

--- softuni-forum/src/app/features/auth/login/login.ts
+++ softuni-forum/src/app/features/auth/login/login.ts
@@ -1,75 +1,105 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { AuthService } from '../../../core/services';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,RouterLink],
+  imports: [ReactiveFormsModule, FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements AfterViewInit {
   private authService = inject(AuthService)
   private router = inject(Router)
+  private fornmBuilder = inject(FormBuilder)
 
-  email: string = ''
-  password: string = ''
-  emailError: boolean = false
-  passwordError : boolean = false
-  emailErrorMessage: string = ''
-  passwordErrorMessage: string = ''
+  loginForm: FormGroup
+
+  constructor() {
+    this.loginForm = this.fornmBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    })
+  }
+  ngAfterViewInit(): void {
+    console.log(this.loginForm);
+    
+  }
+
+  get email() {
+    return this.loginForm.get('email')
+  }
 
 
-  validateEmail(): void{
-    if(!this.email){
-      this.emailError = true
-      this.emailErrorMessage = 'Email is required!'
-    }else if(!this.isValidEmail(this.email)){
-       this.emailError = true
-      this.emailErrorMessage = 'Email is not valid!'
+  get password() {
+    return this.loginForm.get('password')
+  }
 
-    }else{
-      this.emailError = false
-      this.emailErrorMessage = ''
+  get isEmailValid(): boolean {
+    return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false
+  } 
+
+  get isPasswordValid(): boolean{
+    return this.password?.invalid && (this.password?.dirty || this.password?.touched) || false
+  }
+
+  get emailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'Email is required!'
     }
-  }
 
-  validatePassword(): void {
-    if(!this.password) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password is required!';
-    } else if (this.password.length < 4) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password must be at least 4 characters!';    
-    } else {
-      this.passwordError = false;
-      this.passwordErrorMessage = '';
+    if (this.email?.errors?.['email']) {
+      return 'Email is not valid'
     }
+
+    return ''
   }
 
- isFormValid(): boolean {
-    return Boolean(this.email) && Boolean(this.password) && !this.emailError && !this.passwordError;
+  get passwordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'Password is required'
+    }
+
+    if (this.password?.errors?.['minlength']) {
+      return 'Password must be at least 5 characters!'
+    }
+
+    return ''
   }
 
-  onSubmit(): void{
-    this.validateEmail()
-    this.validatePassword()
 
-    if(this.isFormValid()){
-      const response = this.authService.login(this.email,this.password)
+  onSubmit(): void {
+ 
+    if (this.loginForm.valid) {
+      const {email,password} = this.loginForm.value
+      const response = this.authService.login(email,password)
 
-      if(response === true){
+      if (response === true) {
         this.router.navigate(['/home'])
+      } else {
+          this.markFormGroupTouched()
       }
     }
   }
 
-  private isValidEmail(email:string):boolean{
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    return emailRegex.test(email);
+  private markFormGroupTouched(): void {
+    Object.keys(this.loginForm.controls).forEach(key =>{
+      const control = this.loginForm.get(key)
+      control?.markAsTouched()
+    })
   }
 
+  
+}
+export function emailValidator(emailControl: AbstractControl): ValidationErrors | null {
+  const emailRegex = /^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/;
+
+  const email = emailControl.value;
+
+  if (email && !emailRegex.test(email)) {
+    return { emailValidator: true }
+  }
+
+  return null;
 }
