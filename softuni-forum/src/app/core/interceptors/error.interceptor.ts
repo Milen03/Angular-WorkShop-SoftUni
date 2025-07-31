b0c9abfccbca7199b47e20914ec9603f484a43a4
Milen03 <milen.atanasovv03@gmail.com>
2025-07-31 20:24:59 +0300

    update Error interceptor

--- softuni-forum/src/app/core/interceptors/error.interceptor.ts
+++ softuni-forum/src/app/core/interceptors/error.interceptor.ts
@@ -1,7 +1,24 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpErrorResponse, HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { ErrorService } from "../services";
+import { catchError, throwError } from "rxjs";
 
 export const ErrorInterceptor: HttpInterceptorFn =(req,next) =>{
+const errorService = inject(ErrorService)
 
+    return next(req).pipe(
+        catchError((error: HttpErrorResponse) =>{
+            let errorMessage = 'An error occure'
 
-    return next(req)
+            if(error.error instanceof ErrorEvent){
+                //Client side error
+                errorMessage = error.error.message
+            }else{
+                errorMessage = error.error?.message || error.error
+            }
+
+            errorService.setError(errorMessage)
+            return throwError(()=> error)
+        })
+    )
 }
