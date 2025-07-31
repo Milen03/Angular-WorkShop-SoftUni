a013d7ac2596f62e71e42feebaab4d43fe0f01ee
Milen03 <milen.atanasovv03@gmail.com>
2025-07-31 19:30:27 +0300

    logn and argister auth is ready

--- softuni-forum/src/app/core/services/auth.service.ts
+++ softuni-forum/src/app/core/services/auth.service.ts
@@ -1,38 +1,21 @@
 import { Injectable, signal } from "@angular/core";
-import { User } from "../../models";
+import { ApiUser, User } from "../../models";
+import { HttpClient } from "@angular/common/http";
+import { map, Observable, tap } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class AuthService {
-
+      private apiUrl = 'http://localhost:3000/api'
     private _isLoggeIn = signal<boolean>(false)
     private _currentUser = signal<User | null>(null)
-     private _users: User[] = [
-        {
-            id: '5fa64b162183ce1728ff371d', 
-            username: 'John', 
-            email: 'john.doe@gmail.com',
-            phone: '+359 885 888 888'
-        },
-        {
-            id: '5fa64b162183ce1728ff371e', 
-            username: 'Jane',
-            email: 'john.doe@gmail.com',
-            phone: '+359 885 888 888'
-        },
-        {
-            id: '5fa64b162183ce1728ff371f', 
-            username: 'David',
-            email: 'john.doe@gmail.com',
-            phone: '+359 885 888 888'
-        }
-    ]
+
     public isLoggedIn = this._isLoggeIn.asReadonly()
     public currentUser = this._currentUser.asReadonly()
 
-    constructor() {
+    constructor( private httpClient:HttpClient) {
         const savedUser = localStorage.getItem('currentUser')
         if (savedUser) {
             const user = JSON.parse(savedUser)
@@ -41,44 +24,45 @@ export class AuthService {
         }
     }
 
-    login(email: string, password: string): boolean {
-        if (email && password) {
-            const user = this._users[0]
-            this._currentUser.set(user)
-            this._isLoggeIn.set(true)
+    login(email: string, password: string): Observable<User> {
+        return this.httpClient.post<ApiUser>(`${this.apiUrl}/login`,{ email,password } , {
+            withCredentials: true
+        }).pipe(
+            map(apiUser => this.mapApiUserToUser(apiUser)),
+            tap(user => {
+                this._currentUser.set(user)
+                this._isLoggeIn.set(true)
 
-            localStorage.setItem('currentUser', JSON.stringify(user))
-
-            return true
-        }
-
-        return false
+                localStorage.setItem('currentUser', JSON.stringify(user))
+            })
+        )
     }
 
     register(
-        username: string
-        , email: string
-        , password: string,
-        phone: string,
-        rePasword: string): boolean {
-       if(username && email && phone && password && rePasword) {
-            const newUser: User = {
-                id: `user_${Date.now()}`,
-                username: username,
-                email: email,
-                phone: phone
-            };
+        username: string, 
+        email: string,
+         password: string,
+         phone: string,
+         rePasword: string
+        ): Observable<User> {
+            return this.httpClient.post<ApiUser>(`${this.apiUrl}/register`,{
+                username,
+                email,
+                tel:phone,
+                password,
+                rePasword
+            },{
+                withCredentials:true
+            }).pipe(
+                map(apiUser => this.mapApiUserToUser(apiUser)),
+                tap(user =>{
+                    this._currentUser.set(user)
+                    this._isLoggeIn.set(true)
 
-            this._users.push(newUser)
-            this._currentUser.set(newUser)
-            this._isLoggeIn.set(true)
-
-            localStorage.setItem('currentUser', JSON.stringify(newUser))
-
-            return true
-        }
-
-        return false
+                    localStorage.setItem('currentUser', JSON.stringify(user))
+                })
+            )
+      
 
     }
 
@@ -102,5 +86,14 @@ export class AuthService {
         this._currentUser.set(user);
 
         localStorage.setItem('currentUser', JSON.stringify(user));
+    }
+
+    private mapApiUserToUser(apiUser:ApiUser): User{
+        return <User>{
+            id:apiUser._id,
+            username:apiUser.username,
+            email:apiUser.email,
+            phone:apiUser.tel
+        }
     }
 }
